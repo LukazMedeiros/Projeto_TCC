79d83eb999e3a23f0a4bacb87cf50801d2e530c8
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2020-08-31 11:18:41 -0300

    criado controller de criação de usuario, delete e update

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,8 +1,57 @@
 
 module.exports = {
     
-    create(request,response){
-        response.send(`hello world usuario`)
+    async atualizar(request,response){
+        const {
+            cpf,
+            nome,
+            ra
+        } = request.body;
+        // validaçoes
+        if ((cpf==="")||(!cpf)) {
+            return response.status(400).json({mensagem:`CPF não recebido`})
+        }
+        await console.log(`recebido ${cpf} do usuario`) //colocar funçao de atualização para o banco de dados
+        try {
+            return response.status(200).json({mensagem:`cadastro atualizado com sucesso!`})
+        } catch (error) {
+            return response.status(400).json({mensagem:`${error}`})
+        }
+    },
+    
+    async criar(request,response){
+        const {
+            cpf,
+            nome,
+            ra
+        } = request.body;
+        // validaçoes
+        if ((cpf === '')||(!cpf)) {
+            return response.status(400).json({mensagem:`CPF não recebido`})
+        }
+        if ((nome === '')||(!nome)) {
+            return response.status(400).json({mensagem:`nome não recebido`})
+        }
+        await console.log(`funcionou`); //colocar a função de cadastro no banco de dado
+        try {
+            return response.status(200).json({mensagem:`usuário cadastrado com sucesso!`})
+        } catch (error) {
+            return response.status(400).json({mensagem:`${error}`})
+        }
+    },
+    
+    async deletar(request,response){
+        const {naosei} = request.body;
+        // validações
+        if ((naosei==="")||(!naosei)) {
+            return response.status(400).json({mensagem:`verifique os dados informados`})
+        }
+        await console.log(`deletar`);
+        try {
+            return response.status(200).json({mensagem:`usuário excluído com sucesso!`})
+        } catch (error) {
+            return response.status(400).json({mensagem:`${error}`})
+        }
     },
     
 }
