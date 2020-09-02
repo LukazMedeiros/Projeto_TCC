0d990b7f213e9f5d77b7c445859a5339260ed873
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2020-09-02 00:48:15 -0300

    adicionado modulo cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./src/routes/routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
 app.listen(3333, ()=>{
     console.log(`aplicação rodando em http://localhost:3333`);
