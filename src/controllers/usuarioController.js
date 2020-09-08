bbef1f415daa88f8dcdd9328e4f4ce2f78a3bb0a
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2020-09-08 11:23:37 -0300

    adicionando validações em controller de usuarios

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -5,12 +5,21 @@ module.exports = {
         const {
             cpf,
             nome,
-            ra
+            ra,
+            senha
         } = request.body;
+        
         // validaçoes
-        if ((cpf==="")||(!cpf)) {
+        if ((cpf=== '')||(!cpf)) {
             return response.status(400).json({mensagem:`CPF não recebido`})
+        }else if ((nome === '')||(!nome)) {
+            return response.status(400).json({mensagem:`nome não recebido`})
+        } else if((ra === '')||(!ra)){
+            return response.status(400).json({mensagem:`RA não recebido`})
+        }else if((senha === '')||(!senha)){
+            return response.status(400).json({mensagem:`Senha não recebido`})
         }
+        
         await console.log(`recebido ${cpf} do usuario`) //colocar função de atualização para o banco de dados
         try {
             return response.status(200).json({mensagem:`cadastro atualizado com sucesso!`})
@@ -23,15 +32,21 @@ module.exports = {
         const {
             cpf,
             nome,
-            ra
+            ra,
+            senha
         } = request.body;
+        
         // validaçoes
-        if ((cpf === '')||(!cpf)) {
+        if ((cpf==="")||(!cpf)) {
             return response.status(400).json({mensagem:`CPF não recebido`})
-        }
-        if ((nome === '')||(!nome)) {
+        }else if ((nome === "")||(!nome)) {
             return response.status(400).json({mensagem:`nome não recebido`})
+        } else if((ra === "")||(!ra)){
+            return response.status(400).json({mensagem:`RA não recebido`})
+        }else if((senha === "")||(!senha)){
+            return response.status(400).json({mensagem:`Senha não recebido`})
         }
+        
         await console.log(`funcionou`); //colocar a função de cadastro no banco de dado
         try {
             return response.status(200).json({mensagem:`usuário cadastrado com sucesso!`})
@@ -41,14 +56,21 @@ module.exports = {
     },
     
     async deletar(request,response){
-        const {naosei} = request.body;
+        const {
+            usuario, 
+            senha
+        } = request.headers;
+
         // validações
-        if ((naosei==="")||(!naosei)) {
-            return response.status(400).json({mensagem:`verifique os dados informados`})
+        if ((usuario==="")||(!usuario)) {
+            return response.status(400).json({mensagem:`ID do usuário não recebido`})
+        }else if((senha==="")||(!senha)) {
+            return response.status(400).json({mensagem:`Senha do usuário não recebido`})
         }
+
         await console.log(`deletar`);
         try {
-            return response.status(200).json({mensagem:`usuário excluído com sucesso!`})
+            return response.status(200).json({mensagem:`Usuário excluído com sucesso!`})
         } catch (error) {
             return response.status(400).json({mensagem:`${error}`})
         }
